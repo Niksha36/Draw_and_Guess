56cd098ab521c366caafaa4b40f4d61057f1fad2
abbeswrld <a553xxd@gmail.com>
2025-01-12 00:22:00 +1000

    optimization buttons and animation for background

--- frontend/src/components/menu.vue
+++ frontend/src/components/menu.vue
@@ -170,7 +170,7 @@ function goToScore() {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-
+  will-change: auto;
 }
 
 .background {
@@ -238,12 +238,14 @@ function goToScore() {
   height: 120px;
   border: 2px solid #000000;
   border-radius: 50%;
+  will-change: auto;
 }
 
 .avatar-img {
   height: 135px;
   border-radius: 50%;
   object-fit: cover;
+  will-change: auto;
 }
 
 .button {
@@ -261,6 +263,7 @@ function goToScore() {
   cursor: pointer;
   background-position: center;
   box-shadow: none;
+  will-change: auto;
 }
 
 .button:hover {
